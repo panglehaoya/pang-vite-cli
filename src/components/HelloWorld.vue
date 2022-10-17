7f1aaa1c977551a3ade8ddbb996996f07839fc45
panglehaoya <waxxhgz@163.com>
2022-10-17 21:44:57 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  setup() {
+    const count = ref(0)
+    return { count }
+  },
+})
+</script>
